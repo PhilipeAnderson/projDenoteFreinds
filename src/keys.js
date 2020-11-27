f5e5f51043560fd839d36228249f878fe92f5abf
PhilipeAnderson <philipeacampos@hotmail.com>
2020-11-27 20:09:01 -0300

    Enviando para a Umbler II

--- src/keys.js
+++ src/keys.js
@@ -1,16 +1,16 @@
 module.exports = {
 
-    database: {
-        host: 'localhost',
-        user: 'philipe',
-        password: '64919598',
-        database: 'savelinks'
-    }
-
     // database: {
     //     host: 'localhost',
-    //     user: 'root',
-    //     password: '03288731959Pr@',
+    //     user: 'philipe',
+    //     password: '64919598',
     //     database: 'savelinks'
     // }
+
+    database: {
+        host: 'localhost',
+        user: 'root',
+        password: '03288731959Pr@',
+        database: 'savelinks'
+    }
 }
